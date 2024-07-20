8bf9a13749cd47372abd3e1ba38cb3c13934beac
septi <septidwicahyati02@gmail.com>
2024-07-20 11:18:50 +0700

    update

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -9,38 +9,37 @@
                 <a href="{{ route('barang.create') }}" class="btn btn-primary">Add Barang</a>
             </div>
             <div class="table-responsive">
-                <table class="table text-start align-middle table-bordered table-hover mb-0">
+                <table class="table table-bordered table-hover mb-0">
                     <thead class="bg-primary text-white">
                         <tr>
-                            <th>ID</th>
-                            <th>Nama Barang</th>
-                            <th>Merek</th>
-                            <th>Harga</th>
-                            <th>Stok</th>
-                            <th>Satuan</th>
-                            <th>Actions</th>
+                            <th class="text-center" style="width: 5%;">ID</th>
+                            <th style="width: 20%;">Nama Barang</th>
+                            <th style="width: 15%;">Merek</th>
+                            <th style="width: 15%;">Harga</th>
+                            <th style="width: 10%;">Stok</th>
+                            <th style="width: 10%;">Satuan</th>
+                            <th class="text-center" style="width: 25%;">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($barang as $item)
                             <tr>
-                                <td>{{ $item->id_barang }}</td>
-                                <td>{{ $item->nama_barang }}</td>
-                                <td>{{ $item->merek }}</td>
-                                <td>{{ $item->harga }}</td>
-                                <td>{{ $item->stok }}</td>
-                                <td>{{ $item->satuan }}</td>
+                                <td class="text-center">{{ $item->id_barang }}</td>
+                                <td class="text-center">{{ $item->nama_barang }}</td>
+                                <td class="text-center">{{ $item->merek }}</td>
+                                <td class="text-center">{{ $item->harga }}</td>
+                                <td class="text-center">{{ $item->stok }}</td>
+                                <td class="text-center">{{ $item->satuan }}</td>
                                 <td>
-                                    <div class="btn-group">
+                                    <div class="d-flex justify-content-center">
                                         <a href="{{ route('barang.show', $item->id_barang) }}"
-                                            class="btn btn-info btn-sm">View</a>
+                                            class="btn btn-info btn-sm mx-1">View</a>
                                         <a href="{{ route('barang.edit', $item->id_barang) }}"
-                                            class="btn btn-warning btn-sm ml-2">Edit</a>
-                                        <form action="{{ route('barang.destroy', $item->id_barang) }}" method="POST"
-                                            style="display:inline;">
+                                            class="btn btn-warning btn-sm mx-1">Edit</a>
+                                        <form action="{{ route('barang.destroy', $item->id_barang) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm ml-2">Delete</button>
+                                            <button type="submit" class="btn btn-danger btn-sm mx-1">Delete</button>
                                         </form>
                                     </div>
                                 </td>
